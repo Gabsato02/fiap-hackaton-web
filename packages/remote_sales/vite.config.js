71bc1a5ac8a13a37efdbb401924bbadb755dfff3
Luiz Moraes <luizotcunha@hotmail.com>
2025-07-12 09:26:14 -0300

    Refactor: update preview scripts to include host option for consistency across packages

--- packages/remote_sales/vite.config.js
+++ packages/remote_sales/vite.config.js
@@ -3,46 +3,46 @@ import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
-	plugins: [
-		react(),
-		federation({
-			name: 'remote_sales',
-			filename: 'remoteEntry.js',
-			exposes: {
-				'./Sales': './src/presentation/views/Sales',
-			},
-			remotes: {
-				hostApp: 'http://localhost:5000/assets/remoteEntry.js',
-			},
-			shared: [
-				'react',
-				'react-dom',
-				'@mui/material',
-				'@mui/icons-material',
-				'@emotion/react',
-				'@emotion/styled',
-			],
-		}),
-		{
-			name: 'vite-plugin-notify-host-on-rebuild',
-			apply(config, { command }) {
-				return Boolean(command === 'build' && config.build?.watch);
-			},
-			async buildEnd(error) {
-				if (!error) {
-					try {
-						await fetch('http://localhost:5002/__fullReload');
-					} catch (e) {
-						console.log(e);
-					}
-				}
-			},
-		},
-	],
-	build: {
-		modulePreload: false,
-		target: 'esnext',
-		minify: false,
-		cssCodeSplit: false,
-	},
+  plugins: [
+    react(),
+    federation({
+      name: 'remote_sales',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Sales': './src/presentation/views/Sales',
+      },
+      remotes: {
+        hostApp: 'http://localhost:5000/assets/remoteEntry.js',
+      },
+      shared: [
+        'react',
+        'react-dom',
+        '@mui/material',
+        '@mui/icons-material',
+        '@emotion/react',
+        '@emotion/styled',
+      ],
+    }),
+    {
+      name: 'vite-plugin-notify-host-on-rebuild',
+      apply(config, { command }) {
+        return Boolean(command === 'build' && config.build?.watch);
+      },
+      async buildEnd(error) {
+        if (!error) {
+          try {
+            await fetch('http://localhost:5002/__fullReload');
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      },
+    },
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
 });
